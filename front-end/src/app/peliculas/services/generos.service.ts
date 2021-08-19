51fe61612487192b477a0a6e2a1e1fc6906478af
Phantom <jjuancruzrey@gmail.com>
2021-08-18 22:34:07 -0300

    Se implementan variables de entorno y servicio de generos

--- front-end/src/app/peliculas/services/generos.service.ts
+++ front-end/src/app/peliculas/services/generos.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Genero } from '../models/genero';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GenerosService {
 
-  constructor() { }
+  private apiURL = environment.apiURL;
+
+  constructor(private http: HttpClient) { }
+
+  public obtenerTodos(): Observable<Genero[]> {
+    return this.http.get<Genero[]>(`${this.apiURL}/todos`);
+  }
 }
